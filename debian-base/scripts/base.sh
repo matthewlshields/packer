1b8a3d305c81c3718ba969109b8916e102888cc4
Matthew L Shields <geek@shields.cc>
2015-12-31 12:45:10 -0500

    Build taking to long. Attempting to shorten by removing packages.

--- debian-base/scripts/base.sh
+++ debian-base/scripts/base.sh
@@ -2,5 +2,6 @@
 
 # Update the box
 apt-get -y update >/dev/null
-apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+apt-get -y install facter curl
+#linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
 
