92d33a349d5ed091161d8b48cb94820f726167cb
Matthew L Shields <geek@shields.cc>
2015-12-31 10:14:41 -0500

    Install of base packages and Virtualbox Guest Additions

---
+++ debian-base/scripts/base.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Update the box
+apt-get -y update >/dev/null
+apt-get -y install facter linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
+
