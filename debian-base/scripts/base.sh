3a239d771ec0e9fdaeaec073f2f3152df6ad19ca
Matthew L Shields <geek@shields.cc>
2015-12-31 13:16:16 -0500

    Added rsync

--- debian-base/scripts/base.sh
+++ debian-base/scripts/base.sh
@@ -2,6 +2,6 @@
 
 # Update the box
 apt-get -y update >/dev/null
-apt-get -y install facter curl
+apt-get -y install facter curl rsync
 #linux-headers-$(uname -r) build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev curl unzip >/dev/null
 
