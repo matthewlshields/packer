150a675653d797a2cdcf2ead307180c195c3496a
Matthew L Shields <geek@shields.cc>
2016-01-03 11:23:44 -0500

    Added xcfe

--- debian-base/scripts/zerodisk.sh
+++ debian-base/scripts/zerodisk.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set +e
+
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M || true
-sleep 20
 rm -f /EMPTY || true
-sleep 20
 
-set -e
+sync
+sync
+sync
